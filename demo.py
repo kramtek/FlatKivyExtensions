69a8f0c945a63047584baf064f7faa79beef3e63
Michael Kramer <kramtek@gmail.com>
2018-02-27 10:35:39 -0600

    Provide example of specifying a configuration dict in the application configuration entries to generate custom label

--- demo.py
+++ demo.py
@@ -6,11 +6,24 @@ from flat_kivy_extensions.flatappextension import ExtendedFlatApp
 
 from screens import FlatKivyDemoScreen, KivyScreen1, KivyScreen2
 
-app_config_entries = ['MainLabel',
+# Each item in the app_config_entries list must be one of the following:
+#
+#  str():  Label text that should be placed in navigation slide out panel
+#
+#  dict(): Label configuration properties that can be used to construct
+#          the navigation label for customization
+#
+#  tuple(): [0] indicating the text that is applied to the navigation button,
+#           [1] class name of screen to link to
+#           [2] *largs that are passed to screen constructor
+#           [3] **kwargs that are passed to screen constructor
+#
+app_config_entries = ['Main Label',
                       ('FlatKivyDemo Screen', FlatKivyDemoScreen, [], {}),
-                      'Sub Heading',
+                      'SubHeading Label',
                       ('Kivy Screen 1', KivyScreen1, [], {}),
                       ('Kivy Screen 2', KivyScreen2, [], {}),
+                      {'text':'Custom Heading Label', 'style':'NavigationLabelSubHeading', 'color_tuple' : ('Yellow', '500')},
                      ]
 title = 'My App Title'
 about = 'Something to describe the purpose of the application.'
