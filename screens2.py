600ed4b412ff4fc5a0cc56dff50c71a750930b6b
Michael Kramer <kramtek@gmail.com>
2018-03-01 08:41:27 -0600

    Continued testing with custom layouts.

--- screens2.py
+++ screens2.py
@@ -3,6 +3,7 @@
 from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.metrics import dp
 
@@ -13,7 +14,6 @@ Builder.load_string('''
 <-CustomScreen1>:
     title: 'Custom #1'
     theme: ('app', 'screen')
-    style: 'HeaderTitle'
 
     # Button:
     #     text: 'button'
@@ -58,10 +58,10 @@ Builder.load_string('''
     size: dp(10), dp(15)
     widget_size: dp(10), dp(10)
     container_size: dp(15), dp(15)
+    overhead: dp(15)
 
     anchor_x: 'left'
     anchor_y: 'top'
-
 
     BoxLayout:
         orientation: 'vertical' 
@@ -94,10 +94,8 @@ Builder.load_string('''
             BoxLayout:
                 size_hint: None, None
                 size: root.widget_size
-                offset_y: 10 - root.offset
                 offset_x: root.offset
-                additional_offset_y: 10
-                offset_y: 10.0-root.offset + self.additional_offset_y
+                offset_y: root.overhead-root.offset
                 
                 canvas.before:
                     Color:
@@ -109,39 +107,54 @@ Builder.load_string('''
 
                 on_parent: root._add_widget_to_shadow()
 
-    # SomeLayout:
-    #     size: root.widget_size
-
 
 <-SomeLayout>:
     orientation: 'tb-lr'
     size_hint: None, None
-
-    padding: '5dp'
-
+    
+    background_color: (0.0, 0.5, 0.5, 1.0)
+    border_line_width: 2.0
+    border_color: (.1, .1, .1, 0.0)
     radius: 10
     
+    padding: '5dp'
+        
     canvas.before:
         Color:
-            rgba: (0.0, 0.5, 0.5, 1.0)
+            rgba: self.background_color
         RoundedRectangle:
             size: self.size
             pos: self.pos
             radius: [dp(self.radius),dp(self.radius),dp(self.radius),dp(self.radius)]
         Color:
-            rgba: (.1, .1, .1, 1.0)
+            rgba: self.border_color
         Line
             rounded_rectangle: [self.x, self.y, self.width, self.height, dp(self.radius)]
-            width: dp(1)
+            width: dp(self.border_line_width)
 
     FlatButton:
-        text: 'button'
+        text: 'button 1'
         theme: ('green', 'accent')
         size_hint_y: None
         height: '50dp'
 
+<_WidgetContainerLayout>:
+    # canvas.before:
+    #     Color:
+    #         rgba: (1.0, 0.5, 1.0, 1.0)
+    #     Rectangle:
+    #         size: self.size
+    #         pos: self.pos
+    
 ''')
 
+class _WidgetContainerLayout(BoxLayout):
+    ''' Dummy layout to wrap a widget of fixed size and ensure that
+    the widget is centerd horizontally.  Since it is assumed that
+    All widgets added to a CustomScreen are fixed height then the
+    container layout holding the widget is forced to have the same height
+    '''
+    pass
 
 class DropShadowLayout(AnchorLayout):
     def __init__(self, widget, *largs, **kwargs):
@@ -154,12 +167,57 @@ class DropShadowLayout(AnchorLayout):
         self.add_widget(self.widget)
 
     def _resize(self, instance, value):
-        self.size = (instance.width, instance.height+dp(20))
-        self.container_size = (instance.width + dp(20), instance.height + dp(20))
+        self.size = (instance.width, instance.height+dp(25))
+        overhead = 20
+        self.container_size = (instance.width + dp(overhead), instance.height + dp(overhead))
+        self.overhead = overhead
         self.widget_size = instance.size
 
+
 class SomeLayout(StackLayout):
-    pass
+
+    def __init__(self, *largs, **kwargs):
+        super(SomeLayout, self).__init__(*largs, **kwargs)
+
+    def _calculate_minimum_height(self):
+        min_height = 2*self.padding[0]
+        for child in self.children:
+            min_height += child.height + self.spacing[0]
+        return min_height
+
+    def _center(self, instance, value):
+        if instance.parent is not None:
+            if isinstance(instance.parent, _WidgetContainerLayout):
+                instance.center_x = instance.parent.center_x
+
+    def add_widget(self, *largs, **kwargs):
+        # super(SomeLayout, self).add_widget(*largs, **kwargs)
+
+        widget = largs[0];
+
+        container = _WidgetContainerLayout()
+        container.size_hint_y = None
+        widget.container = container
+        widget.bind(height=container.setter('height'))
+        container.height = widget.height
+        widget.bind(pos=self._center)
+        container.add_widget(widget)
+        super(SomeLayout, self).add_widget(container)
+
+        minimum_height_required = self._calculate_minimum_height()
+        self.height = minimum_height_required
+        print 'added widget: minimum height required for layout: %f' % float(minimum_height_required)
+
+    def remove_widget(self, widget):
+        # super(SomeLayout, self).remove_widget(widget)
+        container = widget.container
+        container.remove_widget(widget)
+        super(SomeLayout, self).remove_widget(container)
+        minimum_height_required = self._calculate_minimum_height()
+        self.height = minimum_height_required
+        print 'removed widget: minimum height required for layout: %f' % float(minimum_height_required)
+
+
 
 class CustomScreen1(CustomScreen):
     def __init__(self, *largs, **kwargs):
@@ -173,22 +231,31 @@ class CustomScreen1(CustomScreen):
         self.ds1.shadow_color = (0.1, 0.5, 0.1, 0.8)
         self.add_widget(self.ds1)
 
-
         btn = Button(text='test', size_hint_y=None, height=dp(50))
         self.add_widget(btn)
         btn.bind(on_release=self._btn_released)
         btn.bind(on_press=self._btn_pressed)
 
-        myWidget = SomeLayout()
-        myWidget.size = (dp(300), dp(200))
-        self.ds2 = DropShadowLayout(myWidget)
+        self.myWidget = SomeLayout()
+        self.myWidget.size = (dp(250), dp(200))
+
+        btn = Button(text='test2', size_hint_y=None, height=dp(50))
+        self.myWidget.add_widget(btn)
+        btn = Button(text='test3', size_hint_y=None, height=dp(50), size_hint_x=None, width=dp(150))
+        self.myWidget.add_widget(btn)
+        btn = Button(text='test4', size_hint_y=None, height=dp(100))
+        self.myWidget.add_widget(btn)
+
+        self.ds2 = DropShadowLayout(self.myWidget)
         self.ds2.blur_rad = 8
         self.ds2.offset = 12
         self.ds2.radius = 10
 
-        # myWidget.height = dp(101)
-
         self.add_widget(self.ds2)
+
+        # TODO: fix the issue w/ the shadow widget height not getting
+        #       corrected when the widget height changes
+        # self.myWidget.height = dp(200)
 
     def _btn_pressed(self, instance):
         # Question: can the offset and blur_rad properties
@@ -198,7 +265,6 @@ class CustomScreen1(CustomScreen):
         self.ds2.blur_rad = 4
 
         self.ds1.offset = 1
-        print 's: %s' % str(self.ds1.widget_size)
 
     def _btn_released(self, instance):
         self.ds2.blur_rad = 8
