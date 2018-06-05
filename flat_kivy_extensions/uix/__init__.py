1967b7ec00984655c2ee37642cadff9b73e39840
Michael Kramer <kramtek@gmail.com>
2018-06-05 14:44:52 -0500

    Added layout for custom busy display - popup with a spinner and message

--- flat_kivy_extensions/uix/__init__.py
+++ flat_kivy_extensions/uix/__init__.py
@@ -1,9 +1,18 @@
 
 
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
+from flat_kivy.uix.behaviors import ThemeBehavior
 
-class CustomErrorContent(GridLayout):
+class CustomErrorContent(GridLayout, ThemeBehavior):
     error_text = StringProperty('Default Error Text')
+    label_color_tuple = ListProperty( ['Blue', '800'] )
+
+class CustomBusyContent(GridLayout, ThemeBehavior):
+    busy_text = StringProperty('Default Busy Text')
+    label_color_tuple = ListProperty( ['Blue', '800'] )
+
+    def __init__(self, cancel_callback=None, **kwargs):
+        super(CustomBusyContent, self).__init__(**kwargs)
 
 
