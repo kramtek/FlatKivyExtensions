8000fd1c9c851e1d38a829f463f6df171370fe08
Michael Kramer <kramtek@gmail.com>
2018-08-29 08:49:17 -0500

    Customize threading option so that exceptions caught in separate can be displayed to the UI and off option to show the busy spinning wheel in the application header while threaded process is running

--- flat_kivy_extensions/__init__.py
+++ flat_kivy_extensions/__init__.py
@@ -1,6 +1,8 @@
 __version__ = '0.0.1'
 
-import logging, datetime
+import logging, datetime, threading
+from kivy.app import App
+
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
@@ -25,4 +27,31 @@ class PackageLogger(object):
 
     def error(self, message):
         log.error('%s: %s %s' % (self.moduleName, self._formattedNow(), message))
+
+
+class AppAwareThread(threading.Thread):
+
+    def __init__(self, **kwargs):
+        self._show_busy = kwargs.get('show_busy', False)
+        if 'show_busy' in kwargs.keys():
+            del kwargs['show_busy']
+        super(AppAwareThread, self).__init__(**kwargs)
+        self.app = App.get_running_app()
+        self.daemon = True
+        self._target = kwargs.get('target')
+
+    def run(self):
+        try:
+            if self._show_busy:
+                App.get_running_app().indicate_busy(True)
+            super(AppAwareThread, self).run()
+            if self._show_busy:
+                App.get_running_app().indicate_busy(False)
+        except Exception as e:
+            targetString = str(self._target).strip().split(' ')[2]
+            App.get_running_app().raise_error('Exception:', 'Exception in %s\n\n%s' % (targetString, str(e)),
+                                          auto_dismiss=False)
+            if self._show_busy:
+                App.get_running_app().indicate_busy(False)
+
 
