3b9e14369243298a8a8ede560f5093262df1cfcf
Michael Kramer <kramtek@gmail.com>
2018-09-09 08:43:39 -0500

    rename expected application configuration file to 'local_app_config.xml' and add utility method to check if it even exists.

--- flat_kivy_extensions/utils.py
+++ flat_kivy_extensions/utils.py
@@ -22,15 +22,28 @@ local_font_path = os.path.join(os.path.dirname(os.path.abspath(flat_kivy_extensi
                                         *['data', 'font'])
 relative_path_to_fonts = get_relative_font_path(local_font_path)
 
+from . import PackageLogger
+log = PackageLogger(__name__, moduleDebug=True)
+
+def is_app_config_available():
+    filename = 'local_app_config.xml'
+    try:
+        tree = ET.ElementTree(file=filename)
+        current_root = tree.getroot()
+    except Exception as e:
+        log.error('Exception: %s  (returning None)' % str(e))
+        return False
+    return True
+
 def get_app_config_entry(configTag):
     forceReload = True
     if forceReload:
-        filename = 'app_config.xml'
+        filename = 'local_app_config.xml'
         try:
             tree = ET.ElementTree(file=filename)
             current_root = tree.getroot()
         except Exception as e:
-            print 'Exception: %s  (returning None)' % str(e)
+            log.error('Exception: %s  (returning None)' % str(e))
             return None
     return current_root.find(configTag)
 
@@ -41,12 +54,12 @@ def prettify(elem):
     return reparsed.toprettyxml(indent="  ")
 
 def set_app_config_entry(configTags, tags, attributes):
-    filename = 'app_config.xml'
+    filename = 'local_app_config.xml'
     try:
         tree = ET.ElementTree(file=filename)
         current_root = tree.getroot()
     except Exception as e:
-        print 'Exception: %s  (returning None)' % str(e)
+        log.error('Exception: %s  (returning None)' % str(e))
         return None
 
     element = current_root
