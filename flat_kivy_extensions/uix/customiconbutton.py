fcf435dd34d3a60a00f34ff4789ee06825cd042b
Michael Kramer <kramtek@gmail.com>
2018-09-09 08:44:40 -0500

    Added to support to make sure that horizontal alignment of text and icon for a custom icon button works right

--- flat_kivy_extensions/uix/customiconbutton.py
+++ flat_kivy_extensions/uix/customiconbutton.py
@@ -1,9 +1,25 @@
 
-from kivy.properties import NumericProperty
+from kivy.metrics import dp
+
+from kivy.properties import NumericProperty, StringProperty
 from flat_kivy.uix.flaticonbutton import FlatIconButton
 
 class CustomIconButton(FlatIconButton):
     icon_font_size = NumericProperty(5)
+    orientation = StringProperty('lr-tb')
     pass
+
+    def on_orientation(self, instance, value):
+        if value.startswith('rl') or value.startswith('lr'):
+            self._label.halign = 'left'
+        else:
+            self._label.halign = 'center'
+            self._label.size_hint = (1.0, None)
+            self._label.height = dp(25)
+
+            self._icon.halign = 'center'
+            self._icon.size_hint = (1.0, None)
+            self._icon.height = dp(25)
+
 
 
